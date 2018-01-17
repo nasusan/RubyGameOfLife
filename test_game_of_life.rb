407880e26adffe904ee6f0ebe52ab9f54038d852
nasusan <watanabexy@gmail.com>
2018-01-17 18:21:17 +0900

    初期化テストを追加した

--- test_game_of_life.rb
+++ test_game_of_life.rb
@@ -2,7 +2,14 @@
 
 require 'test/unit'
 
-
 class TestGameOfLife < Test::Unit::TestCase
-
+  #test '実行' do
+  def TestSinglotonInstanse
+    field = Field.instance
+	assert_not_nil field
+  end
+  def TestInittialize
+    assert_equal 1,1
+  end
 end
+
